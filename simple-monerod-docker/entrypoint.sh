f350be2c0766374d164da2f62fbe50e5d5161f14
Lukas Bieringer <bieranger@gmail.com>
2024-07-13 17:08:53 +0200

    added monerod as local build removed noninteractive tag from entrypoint this is the reason i are building this myself

---
+++ simple-monerod-docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Credit for the bulk of this entrypoint script goes to cornfeedhobo
+# Source is https://github.com/cornfeedhobo/docker-monero/blob/master/entrypoint.sh
+set -e
+
+# Configure NUMA if present for improved performance
+if command -v numactl >/dev/null 2>&1; then
+    numa="numactl --interleave=all"
+    set -- "$numa" "$@"
+fi
+# Start the daemon using fixuid
+# to adjust permissions if needed
+exec fixuid -q "$@"
